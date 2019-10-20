4858f15ddd5e6314e05df23ee83c54b5a3653936
Timo Jarmala <timo.jarmala@myy.haaga-helia.fi>
2019-10-20 18:15:44 +0300

    code fixxing

--- dolistfrontend/src/service/toDo.js
+++ dolistfrontend/src/service/toDo.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getTodoList= async () => {
   try {
-    const ret = axios.get(`${process.env.REACT_APP_API}/todo`);
+    const ret = await axios.get(`${process.env.REACT_APP_API}/todo`);
     console.log(ret, "doDossss");
     return ret.data;
   }
