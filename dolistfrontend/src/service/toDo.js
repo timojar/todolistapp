c5e22b277ee193604e7da8e7dd3fd836c6abfa0c
Timo Jarmala <timo.jarmala@myy.haaga-helia.fi>
2019-10-20 17:49:05 +0300

    env variables

--- dolistfrontend/src/service/toDo.js
+++ dolistfrontend/src/service/toDo.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getTodoList= async () => {
   try {
-    const ret = await axios.get(`http://localhost:8888/todo`);
+    const ret = axios.get(`${process.env.REACT_APP_API}/todo`);
     console.log(ret, "doDossss");
     return ret.data;
   }
